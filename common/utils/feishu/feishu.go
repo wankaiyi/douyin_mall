6f40e1e8b670893706d2d422d547e15642375b33
wankaiyi <wankaiyi123@163.com>
2025-02-01 13:26:07 +0800

    refactor: 添加TLS支持和请求方法通用处理

--- common/utils/feishu/feishu.go
+++ common/utils/feishu/feishu.go
@@ -24,7 +24,7 @@ func SendFeishuAlert(ctx context.Context, url string, text string) {
 	}`, text))
 	result, err := http.Post(ctx, url, map[string]string{
 		"Content-Type": "application/json",
-	}, body)
+	}, body, true)
 	if err != nil {
 		klog.Error("飞书告警异常", "url", url, "请求体", string(body), "错误", err)
 		return
--- common/utils/http/http.go
+++ common/utils/http/http.go
@@ -2,7 +2,11 @@ package http
 
 import (
 	"context"
+	"crypto/tls"
+	"errors"
 	"github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/common/config"
+	"github.com/cloudwego/hertz/pkg/network/standard"
 	"github.com/cloudwego/hertz/pkg/protocol"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
@@ -11,56 +15,61 @@ type HTTPClient struct {
 	client *client.Client
 }
 
-func newHTTPClient() (*HTTPClient, error) {
-	c, err := client.NewClient()
+func newHTTPClient(useTLS bool) (*HTTPClient, error) {
+	var options []config.ClientOption
+	if useTLS {
+		options = append(options, client.WithTLSConfig(&tls.Config{
+			InsecureSkipVerify: true,
+		}))
+	}
+	options = append(options, client.WithDialer(standard.NewDialer()))
+
+	c, err := client.NewClient(options...)
 	if err != nil {
 		return nil, err
 	}
 	return &HTTPClient{client: c}, nil
 }
 
-func Get(ctx context.Context, url string, headers map[string]string) (*protocol.Response, error) {
-	hc, err := newHTTPClient()
+func doRequest(ctx context.Context, method, url string, headers map[string]string, body []byte, useTLS bool) (*protocol.Response, error) {
+	hc, err := newHTTPClient(useTLS)
 	if err != nil {
 		return nil, err
 	}
 	req := protocol.AcquireRequest()
+	defer protocol.ReleaseRequest(req)
 	res := protocol.AcquireResponse()
+	defer protocol.ReleaseResponse(res)
+
 	req.SetRequestURI(url)
-	req.Header.SetMethod(consts.MethodGet)
+	req.Header.SetMethod(method)
 
 	for key, value := range headers {
 		req.Header.Set(key, value)
+	}
+
+	if method == consts.MethodPost && body != nil {
+		req.SetBody(body)
 	}
 
 	err = hc.client.Do(ctx, req, res)
 	if err != nil {
 		return nil, err
 	}
+
 	return res, nil
 }
 
-func Post(ctx context.Context, url string, headers map[string]string, body []byte) (*protocol.Response, error) {
-	hc, err := newHTTPClient()
-	if err != nil {
-		return nil, err
+func Get(ctx context.Context, url string, headers map[string]string, useTLS bool) (*protocol.Response, error) {
+	if url == "" {
+		return nil, errors.New("URL cannot be empty")
 	}
-	//var postArgs protocol.Args
-	//postArgs.Set("name", "cloudwego") // Set post args
-	//status, responseBody, err := hc.client.Post(context.Background(), nil, "http://localhost:8080/hello", &postArgs)
-	req := protocol.AcquireRequest()
-	res := protocol.AcquireResponse()
-	req.SetRequestURI(url)
-	req.Header.SetMethod(consts.MethodPost)
-	req.SetBody(body)
+	return doRequest(ctx, consts.MethodGet, url, headers, nil, useTLS)
+}
 
-	for key, value := range headers {
-		req.Header.Set(key, value)
+func Post(ctx context.Context, url string, headers map[string]string, body []byte, useTLS bool) (*protocol.Response, error) {
+	if url == "" {
+		return nil, errors.New("URL cannot be empty")
 	}
-
-	err = hc.client.Do(ctx, req, res)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
+	return doRequest(ctx, consts.MethodPost, url, headers, body, useTLS)
 }
