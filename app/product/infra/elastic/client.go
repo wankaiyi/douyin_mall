28a95695bef2132b59cb8c48a2e35f06bd7bd69b
chenzhihao <939832920@qq.com>
2025-02-02 23:17:31 +0800

    feat:es客户端赋值

--- app/product/infra/elastic/client.go
+++ app/product/infra/elastic/client.go
@@ -12,11 +12,16 @@ var (
 
 func InitClient() {
 	elasticsearchConf := conf.GetConf().Elasticsearch
-	ElasticClient, err := elasticsearch7.NewClient(elasticsearch7.Config{
+	var client *elasticsearch7.Client
+	client, err := elasticsearch7.NewClient(elasticsearch7.Config{
 		Addresses: []string{"http://" + elasticsearchConf.Host + ":" + elasticsearchConf.Port},
 		Username:  elasticsearchConf.Username,
 		Password:  elasticsearchConf.Password,
 	})
-	hlog.Info(ElasticClient)
-	hlog.Error(err)
+	ElasticClient = *client
+	if err != nil {
+		hlog.Error(err)
+		return
+	}
+
 }
