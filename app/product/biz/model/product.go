0b66de42b142184f06e92bc7969d37cb5700b57d
chenzhihao <939832920@qq.com>
2025-02-03 15:52:40 +0800

    feat: 添加创建时间和更新时间字段

--- app/product/biz/model/product.go
+++ app/product/biz/model/product.go
@@ -1,13 +1,17 @@
 package model
 
+import "time"
+
 type Product struct {
-	ID          int64   `gorm:"primary_key" json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Picture     string  `json:"picture"`
-	Price       string  `json:"price"`
-	Stock       int64   `json:"stock"`
-	Sale        float32 `json:"sale"`
-	PublicState int8    `json:"public_state"`
-	LockStock   int64   `json:"lock_stock"`
+	ID          int64     `gorm:"primary_key" json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Picture     string    `json:"picture"`
+	Price       string    `json:"price"`
+	Stock       int64     `json:"stock"`
+	Sale        float32   `json:"sale"`
+	PublicState int8      `json:"public_state"`
+	LockStock   int64     `json:"lock_stock"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
