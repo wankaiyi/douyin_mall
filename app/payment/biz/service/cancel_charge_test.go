f5e5f5bbbe2dcd998131d811d4f0a7251d02eed3
Newman <2022410106@qq.com>
2025-03-08 19:10:14 +0800

    fix：修改了判断取消支付状态逻辑

--- app/payment/biz/service/cancel_charge_test.go
+++ app/payment/biz/service/cancel_charge_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"douyin_mall/payment/biz/dal/alipay"
 	payment "douyin_mall/payment/kitex_gen/payment"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"testing"
@@ -11,9 +12,9 @@ func TestCancelCharge_Run(t *testing.T) {
 	ctx := context.Background()
 	s := NewCancelChargeService(ctx)
 	// init req and assert value
-
+	alipay.Init()
 	req := &payment.CancelChargeReq{
-		OrderId: "1234567891",
+		OrderId: "12345688891",
 	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
